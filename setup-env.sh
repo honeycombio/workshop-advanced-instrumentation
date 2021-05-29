6e4c97ef09881d7dda5b333fc06b2f49644aea42
Pierre Tessier <pierre@pierretessier.com>
2021-05-28 23:47:10 -0400

    make readme useable

---
+++ setup-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+print_usage() {
+  echo "setup-env.sh <api key> <dataset> [-w]"
+  echo
+  echo "-w option will write settings to user profile"
+}
+
+if [[ -z "$1" ]]; then
+  print_usage
+  exit 1
+fi
+
+if [[ -z "$2" ]]; then
+  print_usage
+  exit 1
+fi
+
+echo "setting up environment"
+export HONEYCOMB_API_KEY="$1"
+export HONEYCOMB_DATASET="$2"
+
+if [[ -n "$3" ]] && [[ "$3" == "-w" ]]; then
+  echo "persisting to user profile"
+  cat >> ~/.profile <<EOL
+export HONEYCOMB_API_KEY="$1"
+export HONEYCOMB_DATASET="$2"
+EOL
+fi
