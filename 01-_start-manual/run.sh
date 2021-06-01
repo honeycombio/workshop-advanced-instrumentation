4902279be9d6e9f049db41d7aaf116772ccbc707
Pierre Tessier <pierre@pierretessier.com>
2021-05-31 21:10:19 -0400

    remove instrumentation from start

--- 01-_start-manual/run.sh
+++ 01-_start-manual/run.sh
@@ -18,15 +18,10 @@ java_year() {
 
   gradle bootJar
 
-  export OTEL_METRICS_EXPORTER="none"
-  export OTEL_EXPORTER_OTLP_ENDPOINT="https://api.honeycomb.io"
-  export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=${HONEYCOMB_API_KEY},x-honeycomb-dataset=${HONEYCOMB_DATASET}"
-  export OTEL_RESOURCE_ATTRIBUTES="service.name=java-year"
-
   if [[ -n "$2" ]] && [[ "$2" == "-b" ]]; then
-    java -javaagent:../../lib/opentelemetry-javaagent-all.jar -jar build/libs/java-year.jar &
+    java -jar build/libs/java-year.jar &
   else
-    java -javaagent:../../lib/opentelemetry-javaagent-all.jar -jar build/libs/java-year.jar
+    java -jar build/libs/java-year.jar
   fi
 }
 
