e75984d773872ee5b91ff05c8beb8b4d47a2fbde
sgsharma <sashasharma@honeycomb.io>
2022-03-01 18:20:32 -0800

    updated instrumentation

--- 01-manual/python-year/python-year.py
+++ 01-manual/python-year/python-year.py
@@ -2,14 +2,15 @@ import asyncio
 import os
 import random
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
     OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (BatchSpanProcessor,
-                                            ConsoleSpanExporter)
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# from opentelemetry.sdk.trace.export import ConsoleSpanExporter
 
 app = FastAPI()
 
@@ -19,36 +20,46 @@ resource = Resource(attributes={
     "service.name": "python-year"
 })
 
-# create the OTLP exporter to send data an insecure OpenTelemetry Collector
-otlp_exporter = OTLPSpanExporter(endpoint="https://api.honeycomb.io", insecure=True)
+# create the OTLP exporter to send data to an insecure OpenTelemetry Collector
+otlp_exporter = OTLPSpanExporter(
+    endpoint="grpc://api.honeycomb.io", 
+    insecure=True,
+    headers=(
+        ('x-honeycomb-team', os.getenv("HONEYCOMB_API_KEY", "")),
+        ('x-honeycomb-dataset', os.getenv("HONEYCOMB_DATASET", ""))
+        )
+    )
 
-trace.set_tracer_provider(TracerProvider())
+trace.set_tracer_provider(TracerProvider(resource=resource))
 tracer = trace.get_tracer_provider().get_tracer(__name__)
 trace.get_tracer_provider().add_span_processor(
+    # If you'd like to see the OTel data in the console, use the ConsoleSpanExporter
+    # BatchSpanProcessor(ConsoleSpanExporter())
     BatchSpanProcessor(otlp_exporter)
 )
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/year")
-async def year(request: Request):
-    with tracer.start_as_current_span("getYear"):
-        # divide by 1000 to convert to milliseconds
-        rnd = getRandomInt(250)
-        await asyncio.sleep(rnd/1000)
-        result = determineYear()
-        return result
-
 
 def getRandomInt(max):
     return random.randint(1, max)
 
-
-def determineYear():
+async def determineYear():
     years = [2015, 2016, 2017, 2018, 2019, 2020]
-    # get a random element from the list of years
-    year = random.choice(years)
-    return year
+    # Attach a new child and update the current span
+    with tracer.start_span("getYear"):
+        rnd = getRandomInt(250)
+        # divide by 1000 to convert to milliseconds
+        await asyncio.sleep(rnd/1000)
+        # get a random element from the list of years
+        year = random.choice(years)
+        return year
+
+
+# App
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/year")
+async def year(request: Request, status_code=status.HTTP_200_OK):
+    with tracer.start_as_current_span("/year"):
+        result = await determineYear()
+        return {"year": result}
