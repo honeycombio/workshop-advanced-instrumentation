01cd51c6e9ba92fd8a048bbf2fda4435120d1d8b
sgsharma <sashasharma@honeycomb.io>
2022-03-10 22:09:54 -0800

    add psf-black for python linting

--- 01-_start-manual/python-year/python-year.py
+++ 01-_start-manual/python-year/python-year.py
@@ -6,6 +6,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+def home():
+    return {"Hello": "World"}
+
+
 @app.get("/year")
 async def year():
     result = await get_year()
@@ -19,7 +24,7 @@ def get_random_int(max):
 async def get_year():
     years = [2015, 2016, 2017, 2018, 2019, 2020]
     rnd = get_random_int(250)
-    await asyncio.sleep(rnd/1000)
+    await asyncio.sleep(rnd / 1000)
     # get a random element from the list of years
     year = random.choice(years)
     return year
