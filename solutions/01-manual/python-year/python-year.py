555bd1bf4db5ee4a725397c324c58ead69572a83
Michael Sickles <michael.c.sickles@gmail.com>
2022-12-21 14:49:05 -0500

    fixing pythong

--- solutions/01-manual/python-year/python-year.py
+++ solutions/01-manual/python-year/python-year.py
@@ -1,18 +1,9 @@
-import asyncio
+import time
 import random
 
 from fastapi import FastAPI
 
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
-
-# setup trace provider
-tracer_provider = TracerProvider()
-trace.set_tracer_provider(tracer_provider)
-tracer_provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
 
 tracer = trace.get_tracer(__name__)
 
@@ -23,26 +14,28 @@ app = FastAPI()
 async def year():
     span = trace.get_current_span()
     span.set_attribute("foo", "bar")
-    result = await get_year()
+    result = get_year()
     return result
 
 
 def get_random_int(max):
     return random.randint(1, max)
 
-
-async def get_year():
+@tracer.start_as_current_span("random-year")
+def get_year():
+    do_some_work()
     years = [2015, 2016, 2017, 2018, 2019, 2020]
-    # Start a new child span
-    with tracer.start_as_current_span("getYear") as span:
-        rnd = get_random_int(250)
-        span.set_attribute("random-index", rnd)
-
-        # divide by 1000 to convert to milliseconds
-        await asyncio.sleep(rnd / 1000)
-
-        # get a random element from the list of years
-        year = random.choice(years)
-        span.set_attribute("random-year", year)
+    # get a random element from the list of years
+    year = random.choice(years)
+    span = trace.get_current_span()
+    span.set_attribute("random-year", year)
 
     return year
+
+def do_some_work():
+    with tracer.start_as_current_span("some-work") as span:
+        span.set_attribute("otel", "rocks")
+        rnd = get_random_int(250)
+        span.set_attribute("random-sleep", rnd)
+        time.sleep(rnd / 1000)
+
