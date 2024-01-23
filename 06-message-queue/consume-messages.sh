a6269fe84ffd311b839ee3c420fd869b477e0d53
Michael Sickles <michaelsickles@honeycomb.io>
2024-01-23 14:56:46 -0500

    switch to otel java agent

--- 06-message-queue/consume-messages.sh
+++ 06-message-queue/consume-messages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export JAVA_TOOL_OPTIONS="-javaagent:../lib/honeycomb-opentelemetry-javaagent.jar"
+export JAVA_TOOL_OPTIONS="-javaagent:../lib/opentelemetry-javaagent.jar"
 export OTEL_TRACES_EXPORTER="otlp"
 export OTEL_METRICS_EXPORTER="none"
 export OTEL_EXPORTER_OTLP_ENDPOINT="https://api.honeycomb.io"
 export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=$HONEYCOMB_API_KEY"
-export SERVICE_NAME="kafka-consumer"
+export OTEL_SERVICE_NAME="kafka-consumer"
 
 # Compile the project
 mvn clean compile
--- 06-message-queue/produce-messages.sh
+++ 06-message-queue/produce-messages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export JAVA_TOOL_OPTIONS="-javaagent:../lib/honeycomb-opentelemetry-javaagent.jar"
+export JAVA_TOOL_OPTIONS="-javaagent:../lib/opentelemetry-javaagent.jar"
 export OTEL_TRACES_EXPORTER="otlp"
 export OTEL_METRICS_EXPORTER="none"
 export OTEL_EXPORTER_OTLP_ENDPOINT="https://api.honeycomb.io"
 export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=$HONEYCOMB_API_KEY"
-export SERVICE_NAME="kafka-producer"
+export OTEL_SERVICE_NAME="kafka-producer"
 
 # Compile the project
 mvn clean compile
