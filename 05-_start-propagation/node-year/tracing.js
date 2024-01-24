5ce8c49d0c82e2e8bf53d1753c388cc1d9153d51
sgsharma <sashasharma@honeycomb.io>
2024-01-23 22:31:05 -0800

    remove fs instrumentation

--- 05-_start-propagation/node-year/tracing.js
+++ 05-_start-propagation/node-year/tracing.js
@@ -5,7 +5,13 @@ const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-grpc'
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter(),
-  instrumentations: [ getNodeAutoInstrumentations() ]
+    instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-fs': {
+        enabled: false,
+      },
+    }),
+  ],
 })
 
 sdk.start()
