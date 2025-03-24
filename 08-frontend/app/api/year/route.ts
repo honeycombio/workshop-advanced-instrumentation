f572842c26ff3fbb6a46d7ad4291dfce34b39d5c
howardyoo <howardyoo@gmail.com>
2025-03-24 11:19:18 -0500

    adding custom logging instrumentation to frontend

--- 08-frontend/app/api/year/route.ts
+++ 08-frontend/app/api/year/route.ts
@@ -1,18 +1,23 @@
 import { NextResponse } from "next/server";
+import { logger } from "@/instrumentation";
 
 export async function GET() {
   const host = process.env.NEXT_PUBLIC_HOST || "localhost";
   try {
     console.log("Fetching year data");
+    logger.info("Fetching year data");
     const response = await fetch(`http://${host}:6001/year`);
-    console.log(`Response: ${response}`);
     if (!response.ok) {
+      logger.error(`Error: ${response.statusText}`);
       throw new Error(`Error: ${response.statusText}`);
     }
 
     const data = await response.json();
+    console.log(`Response: ${JSON.stringify(data, null, 2)}`);
+    logger.debug(`Response: ${JSON.stringify(data, null, 2)}`);
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
+    logger.error("Failed to fetch data", { error });
     return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
   }
 }
